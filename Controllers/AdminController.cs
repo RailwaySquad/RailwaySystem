f2c2db2a7773e4777154de5153d6a3b7619aa9e9
quan0511 <minhquankb2003@gmail.com>
2023-11-26 13:02:48 +0700

    CreateAdminController

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Railway_Group01.Controllers
+{
+	public class AdminController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
