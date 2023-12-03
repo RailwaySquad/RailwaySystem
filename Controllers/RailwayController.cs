b86bb83211bd97b8bd56c9bce0b2262fc92a76c8
Tu <tuminhvo98@gmail.com>
2023-12-03 14:10:33 +0700

    build fine

--- Controllers/RailwayController.cs
+++ Controllers/RailwayController.cs
@@ -55,21 +55,15 @@ namespace Railway_Group01.Controllers
                 item.Route = await ctx.Routes!.FindAsync(item.RouteId);
                 item.Route.StartStation = await ctx.Stations.FindAsync(item.Route.StartStationId);
                 item.Route.EndStation = await ctx.Stations.FindAsync(item.Route.EndStationId);
-                List<Coach> coaches =await ctx.Coachs.Where(x=>x.TrainCode == item.TrainCode).ToListAsync();
-                item.Train.Coaches = new List<CoachDto>();
+                List<Coach> coaches =await ctx.Coaches.Where(x=>x.TrainCode == item.TrainCode).ToListAsync();
+                item.Train.Coaches = new List<Coach>();
                 foreach (var item1 in coaches)
                 {
-                    item.Train.Coaches.Add(new CoachDto { Id = item1.Id, Name = item1.Name, NoOfSeat = item1.NoOfSeat, Description = item1.Description, NoOfCompartment = item1.NoOfCompartment, TrainCode = item1.TrainCode, SeatAvailable = item1.SeatAvailable, TypeCode = item1.TypeCode });
-                }
-                foreach (var item2 in item.Train.Coaches)
-                {
-                    Fare? fee = await ctx.Fares!.FirstOrDefaultAsync(x => x.TypeOfTrain == item.Train.TypeCode && x.TypeOfClass == item2.TypeCode);
-                    Console.WriteLine(fee.Price);
-                    item2.CoachFare =  (fee.Price+1 )* (fee.DistanceRange * item.Route.Distance)/100;
+                    item.Train.Coaches.Add(new Coach { Id = item1.Id, CoachNo = item1.CoachNo, NoOfSeats = item1.NoOfSeats, NoOfCompartments = item1.NoOfCompartments, TrainCode = item1.TrainCode, Class = item1.Class, Seats = item1.Seats });
                 }
             }
-            IEnumerable<Coach> coaches2 = await ctx.Coachs.ToListAsync();
-            var coach3 = coaches2.DistinctBy(x => x.Name);
+            IEnumerable<Coach> coaches2 = await ctx.Coaches.ToListAsync();
+            var coach3 = coaches2.DistinctBy(x => x.CoachNo);
             var tupleModel = new Tuple<IEnumerable<Schedule>,IEnumerable<Coach>>(schedules,coach3);
             return View(tupleModel);
 
@@ -84,19 +78,19 @@ namespace Railway_Group01.Controllers
             int arrivalId = !String.IsNullOrEmpty(arrival) || int.TryParse(arrival, out _) ? Int32.Parse(arrival) : 4;
             int routeId = await ctx.Routes!.Where(x => x.StartStationId == departureId && x.EndStationId == arrivalId).Select(x=>x.Id).FirstOrDefaultAsync();
             DateTime timeS = !String.IsNullOrEmpty(time) || DateTime.TryParse(time, out _) ? DateTime.Parse(time) : DateTime.Parse("2023-12-2");
-            Schedule? sche = await ctx.Schedules!.Where(x => x.StartAt >= timeS && x.StartAt<= timeS.AddDays(1) && x.RouteId==routeId && x.TrainCode=="SE801").Include(x=>x.Route).FirstAsync();
+            Schedule? sche = await ctx.Schedules!.Where(x => x.Departure >= timeS && x.Arrival<= timeS.AddDays(1) && x.RouteId==routeId && x.TrainCode=="SE801").Include(x=>x.Route).FirstAsync();
             if (sche != null)
             {
                 sche.Route.StartStation = await ctx.Stations!.FindAsync(sche.Route.StartStationId);
                 sche.Route.EndStation = await ctx.Stations!.FindAsync(sche.Route.EndStationId);
-                sche.Route.RouteDetails = await ctx.RouteDetailss!.Where(x => x.RouteId == sche.Route.Id).Include(x=>x.ArrivalStation).Include(x=>x.DepartureStation).ToListAsync();
+                sche.Route.RouteDetails = await ctx.RouteDetails!.Where(x => x.RouteId == sche.Route.Id).Include(x=>x.ArrivalStation).Include(x=>x.DepartureStation).ToListAsync();
             }
             else
             {
                 return RedirectToAction("Error404",new { type = "schedule" });
             }
-            int stop = await ctx.RouteDetailss!.Where(x => x.RouteId == routeId).CountAsync() - 2;
-            Train? train = await ctx.Trains!.Include(x=>x.Schedules).Include(x=>x.CoachesData).FirstOrDefaultAsync(x=>x.TrainCode=="SE801");
+            int stop = await ctx.RouteDetails!.Where(x => x.RouteId == routeId).CountAsync() - 2;
+            Train? train = await ctx.Trains!.Include(x=>x.Schedules).Include(x=>x.Coaches).FirstOrDefaultAsync(x=>x.Code=="SE801");
             if(train == null)
             {
                 return RedirectToAction("Error404", new { type = "train" });
