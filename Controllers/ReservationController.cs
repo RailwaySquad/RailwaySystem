0bc4a56f09f6f8bf7207bf300994a7c7bc9a9475
Tu <tuminhvo98@gmail.com>
2023-12-06 15:03:13 +0700

    build booking

---
+++ Controllers/ReservationController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Railway_Group01.Data;
+using Railway_Group01.Models;
+
+namespace Railway_Group01.Controllers
+{
+    [Authorize(Roles = "Passenger")]
+    public class ReservationController: Controller
+    {
+        RailwayDbContext _ctx;
+
+        public ReservationController(RailwayDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public async Task<IActionResult> Booking()
+        {
+            string? listCart = HttpContext.Session.GetString("listCart");
+            if (listCart != null)
+            {
+                List<CartDto>? carts = JsonConvert.DeserializeObject<List<CartDto>>(listCart);
+                return View(carts);
+            }
+            return View();
+        }
+    }
+}
